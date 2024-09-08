582765468dbb0c39aff85abf2889e629840b610a
Akihiko Sato <asato@live-cast.asia>
2024-09-08 22:09:40 +0900

    ・HelloControllerにメソッドを追加 ・hello.htmlテンプレートを追加

--- hello/src/main/java/com/example/hello/HelloController.java
+++ hello/src/main/java/com/example/hello/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -9,6 +11,12 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+    
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name") String param, Model model) {
+        model.addAttribute("msg", "Hi!" + param);
+        return "hello";
     }
     
 }
