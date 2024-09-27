a4e3856069e5c19792984b8a338a98184e85db9c
Akihiko Sato <asato@live-cast.asia>
2024-09-27 21:25:35 +0900

    ・入力フォームの追加 ・POSTに対応するメソッドを追加

--- hello/src/main/java/com/example/hello/HelloController.java
+++ hello/src/main/java/com/example/hello/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -16,6 +17,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam("name") String param, Model model) {
         model.addAttribute("msg", "Hi!" + param);
+        return "hello";
+    }
+    
+    @PostMapping("/hello")
+    public String helloByPost(@RequestParam("name") String param, Model model) {
+        model.addAttribute("msg", "Bye!" + param);
         return "hello";
     }
     
